2fc1732412cd6a59828ea383bfa2f59c77daa989
Danial Yunus <dyunus@asu.edu>
2019-10-09 18:43:26 -0700

    Initial Commit of AddingMachine.

---
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,63 @@
+/**
+ * @author: Danial Yunus
+ * Student ID: 1212579067
+ * Class ID: CSE360 M 9:40 70641
+ * Assignment Number: 2
+ * Description: Contains the AddingMachine Class which performs basic arithmetic operations
+ */
+package cse360assign2;
+
+public class AddingMachine {
+	
+	private int total;
+	
+	/**
+	 * Constructor which creates an Adding Machine
+	 *  @return AddingMachine Object which performs basic arithmetic operations 
+	 *  and stores a history of operations
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 *  @return returns the current total of the operations performed
+	 */
+	public int getTotal () {
+			return 0;
+		}
+	
+	/**
+	 *  @param takes a value to add
+	 *  @return returns the new total with the value added to existing total
+	 */
+	
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 *  @param takes a value to subtract
+	 *  @return returns the new total with the value subtraced from existing total
+	 */
+	
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 *  @return returns a string with the history of operations performed
+	 */
+	
+	public String toString () {
+		return "";
+		}
+	
+	/**
+	 *  clears the calculator total and history 
+	 */
+	
+	public void clear() {
+		
+	}
+}
